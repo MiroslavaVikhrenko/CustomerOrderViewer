812ae7ca5cddcd0ec23263d544ae031cb1cbfce2
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-05 16:02:02 -0700

    Put all together

--- CustomerOrderViewer/Program.cs
+++ CustomerOrderViewer/Program.cs
@@ -8,11 +8,37 @@ namespace CustomerOrderViewer
     {
         static void Main(string[] args)
         {
-            //instantiate CustomerOrderDetailCommand
-            CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=localhost;Initial Catalog=CustomerOrderViewer;Integrated Security=True;Encrypt=False");
-            
-            //Then from that command use GetList()
-            IList<CustomerOrderDetailModel> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+            try
+            {
+                //instantiate CustomerOrderDetailCommand
+                CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=localhost;Initial Catalog=CustomerOrderViewer;Integrated Security=True;Encrypt=False");
+
+                //Then from that command use GetList()
+                IList<CustomerOrderDetailModel> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+
+                //Any is Linq => says if the list has anything in it
+                if (customerOrderDetailModels.Any())
+                {
+                    //iterate through the list
+                    foreach (CustomerOrderDetailModel customerOrderDetailModel in customerOrderDetailModels)
+                    {
+                        Console.WriteLine("{0}: Fullname: {1} {2} (Id: {3}) - purchased {4} for {5} (Id: {6})",
+                            customerOrderDetailModel.CustomerOrderId,
+                            customerOrderDetailModel.FirstName,
+                            customerOrderDetailModel.LastName,
+                            customerOrderDetailModel.CustomerId,
+                            customerOrderDetailModel.Description,
+                            customerOrderDetailModel.Price,
+                            customerOrderDetailModel.ItemId);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong {0}", ex.Message); //eg, db is down
+            }
+
+
             Console.ReadKey();
         }
     }
